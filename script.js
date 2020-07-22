491639bed8346c24a978a2d60be63f6ed0d9841c
neaz2006 <neaz1986@gmail.com>
2020-07-22 18:26:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    const loginArea = document.getElementById("login-area");   
+    const transactionArea = document.getElementById("transaction-area");   
+
+    loginArea.style.display = "none";
+    transactionArea.style.display = "block";
+
+    
+}
